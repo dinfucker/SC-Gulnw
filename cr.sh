b79df9b0e4b002c195a204686bddf861413a2d3b
jame10000lnw <38467489+jame10000lnw@users.noreply.github.com>
2021-10-22 16:21:18 +0700

    Add files via upload

---
+++ cr.sh
@@ -0,0 +1,11 @@
+echo
+echo "           ꧁༺༂KGUZA༂༻꧂ "
+echo "       ⊹⊱s̴c̴r̴i̴p̴t̴ f̴u̴l̴l̴ f̴u̴n̴c̴t̴i̴o̴n̴ S̴e̴r̴v̴e̴r̴ v̴p̴n̴⊰⊹  "
+echo " ┈༆━━━━┳━━━━━━━━┳━━━━━━━━༆"
+echo " ┈༆━━━━┻━━━━━━━━┻━━━━━━━━༆"
+echo "          ꧁༺ต̡ิ̡ด̡ต่̡̡อ̡ส̡อ̡บ̡ถ̡า̡ม̡༻꧂"
+echo "        http://line.me/ti/p/Dh6o2a5Ar9"
+echo " ┈༆━━━━━━━━━━━━━━━━━━━━━━༆"
+echo " "
+
+
