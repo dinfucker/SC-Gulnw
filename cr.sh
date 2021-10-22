ccdb9bbdb623324d2b41ce13e1f75372a626f356
jame10000lnw <38467489+jame10000lnw@users.noreply.github.com>
2021-10-22 16:26:17 +0700

    Update cr.sh

--- cr.sh
+++ cr.sh
@@ -1,11 +1,10 @@
 echo
-echo "           ꧁༺༂KGUZA༂༻꧂ "
-echo "       ⊹⊱s̴c̴r̴i̴p̴t̴ f̴u̴l̴l̴ f̴u̴n̴c̴t̴i̴o̴n̴ S̴e̴r̴v̴e̴r̴ v̴p̴n̴⊰⊹  "
-echo " ┈༆━━━━┳━━━━━━━━┳━━━━━━━━༆"
-echo " ┈༆━━━━┻━━━━━━━━┻━━━━━━━━༆"
-echo "          ꧁༺ต̡ิ̡ด̡ต่̡̡อ̡ส̡อ̡บ̡ถ̡า̡ม̡༻꧂"
-echo "        http://line.me/ti/p/Dh6o2a5Ar9"
-echo " ┈༆━━━━━━━━━━━━━━━━━━━━━━༆"
+echo "               GULNW  "
+echo "  script-full-function-Servrr-vpn  "
+echo "  -------------------------------  "
+echo "           ติดต่อสอบถามได้  "
+echo "  https://line.me/ti/p/4Y6KvHVfxZ
+echo "  -------------------------------  "
 echo " "
 
 
